e13b4d22c58b5d3ede78c46bccd2c3ddc95286bf
suryanshpsingh <67669995+suryanshpsingh@users.noreply.github.com>
2021-05-25 22:56:04 -0400

    Create firbaseConfig.js

---
+++ firbaseConfig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDwaQgvZwjdkJfw7KOD59MUFzQhsjYq-oU",
+    authDomain: "yardsale-42be8.firebaseapp.com",
+    projectId: "yardsale-42be8",
+    storageBucket: "yardsale-42be8.appspot.com",
+    messagingSenderId: "414562145420",
+    appId: "1:414562145420:web:fc840bfd55f75f90ff97a9",
+    measurementId: "G-XW79NYCRE4"
+  };
+
+export default firebaseConfig;
+
